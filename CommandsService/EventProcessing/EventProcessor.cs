60e08e1d3546b2d4f6183936eafeb01004eebd6a
Mark <g649731852@gmail.com>
2021-10-28 15:09:27 +0800

    add EventProcessor MessageBusSubscriber

---
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text.Json;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactory,IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+        public void ProcessEvent(string message)
+        {
+            var eventType = DetermineEvent(message);
+
+            switch (eventType)
+            {
+                case EventType.PlatformPublished:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private EventType DetermineEvent(string notifcationMessage)
+        {
+            Console.WriteLine("--> Determining Event");
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notifcationMessage);
+
+            switch (eventType.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("Platform Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("--> Could not determine the event type");
+                    return EventType.Undetermined;
+            }
+        }
+
+        private void addPlatform(string platformPublishedMessage)
+        {
+            using (var scope=_scopeFactory.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetRequiredService<ICommandRepo>();
+
+                var platformPublishedDto = JsonSerializer.Deserialize<PlatfordReadDto>(platformPublishedMessage);
+
+                try
+                {
+                    var plat = _mapper.Map<Platform>(platformPublishedDto);
+                    if (!repo.PlatformExits(plat.ExternalID))
+                    {
+                        repo.CreatePlatform(plat);
+                        repo.SaveChange();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"--> Platform already exists...");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Could not add Platform to DB{ex.Message}");
+                }
+            }
+        }
+    }
+    enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+}
--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -1,17 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
+using CommandsService.EventProcessing;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace CommandsService
@@ -30,7 +26,10 @@ namespace CommandsService
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
             services.AddScoped<ICommandRepo, CommandRepo>();
+            services.AddSingleton<IEventProcessor, EventProcessor>();
             services.AddControllers();
+            services.AddHostedService<MessageBusSubscriber>();
+            
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
